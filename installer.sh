2205db87f65353677e0bd35d8af7488dbfe7e053
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2025-04-29 23:05:40 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Installing Admin Panel Fetcher dependencies..." | lolcat
+echo "Installing AdminPBuster dependencies..." | lolcat
 
 apt update
 apt install -y toilet lolcat curl | lolcat
@@ -8,9 +8,9 @@ apt install -y toilet lolcat curl | lolcat
 echo "Installing Python packages..." | lolcat
 pip3 install requests termcolor urllib3 --break-system-packages
 
-echo "Making admin_panel_fetcher.py executable..." | lolcat
-chmod +x admin_panel_fetcher.py
+echo "Making AdminPBuster.py executable..." | lolcat
+chmod +x AdminPBuster.py
 
 echo -e "\nInstallation complete!" | lolcat
-echo "Run tool: ./admin_panel_fetcher.py -t example.com -th 10" | lolcat
-echo "Run with random agent: ./admin_panel_fetcher.py -t example.com -th 10 -ua" | lolcat
+echo "Run tool: ./AdminPBuster -t example.com -th 10" | lolcat
+echo "Run with random agent: ./AdminPBuster.py -t example.com -th 10 -ua" | lolcat
